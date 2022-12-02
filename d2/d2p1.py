0ee3525606af9f81b498da81302e14ae768b6a40
crohan99 <carson.rohan@gmail.com>
2022-12-02 16:25:49 -0600

    Started day 2 part 2

--- d2/d2p1.py
+++ d2/d2p1.py
@@ -1,10 +1,8 @@
 """
 Carson Rohan
 AOC2022
-Name_of_puzzle
+Day 2 Part 1: Rock, paper, scissors, lizard, Spock, modulo
 """
-
-import os
 
 NUM_VARIABLES = 3
 NUM_OUTCOMES = NUM_VARIABLES
@@ -21,10 +19,12 @@ def getTotalScore(rounds: list):
 	return totalScore
 
 def getRoundScore(opponentChoice: chr, myChoice: chr):
-	# choices are ABC -> 123 or XYZ -> 123
-	# difference of choices % 3 gives us [0: draw, 1: myChoice wins, 2: opponentChoice wins]
-	# score multiplier is just 3 so add 1 to this ^ and modulo by multiplier, then multipy by multiplier
-	# finish by adding choice value
+	"""
+	choices are ABC -> 123 or XYZ -> 123
+	difference of choices % 3 gives us [0: draw, 1: myChoice wins, 2: opponentChoice wins]
+	score multiplier is just 3 so add 1 to this ^ and modulo by multiplier, then multipy by multiplier
+	finish by adding choice value
+	"""
 	return ((((((ord(myChoice) - ASCII_X_VALUE) + 1) - ((ord(opponentChoice) - ASCII_A_VALUE) + 1)) % NUM_VARIABLES) + 1) % NUM_OUTCOMES) * NUM_OUTCOMES + ((ord(myChoice) - ASCII_X_VALUE) + 1)
 
 if __name__ == '__main__':
--- d2/d2p2.py
+++ d2/d2p2.py
@@ -1,13 +1,30 @@
 """
 Carson Rohan
 AOC2022
-Name_of_puzzle
+Day 2 Part 2: Rock, paper, scissors, lizard, Spock, modulo
 """
 
-import d2p1
+import d2p1 as d
 
-def main():
+def getTotalScore(rounds: list):
+	totalScore: int = 0
+
+	for round in rounds:
+		choices = round[:-1].split(' ')
+		totalScore += getRoundScore(choices[0], choices[1])
+
+	return totalScore
+
+def getRoundScore(opponentChoice: chr, myChoice: chr):
+	# do this the dumb way first
+	# if 1 then choose (mine-other)%3 == 2 (lose)
+	# if (ord(myChoice) - d.ASCII_X_VALUE == 1):
+
+	# if 2 then choose (mine-other)%3 == 0 (draw)
+
+	# if 3 then choose (mine-other)%3 == 1 (win)
+
 	return 0
 
 if __name__ == '__main__':
-	main()
+	getTotalScore()
