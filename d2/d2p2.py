25f3411381b65cde76b2e930224d61b993626721
crohan99 <carson.rohan@gmail.com>
2022-12-02 13:14:22 -0600

    Completed day 1 part 1

--- d2/d2p2.py
+++ d2/d2p2.py
@@ -1,9 +1,10 @@
-
 """
 Carson Rohan
 AOC2022
 Name_of_puzzle
 """
+
+import d2p1
 
 def main():
 	return 0
--- d2/main.py
+++ d2/main.py
@@ -1,11 +1,23 @@
-
 """
 Carson Rohan
 AOC2022
 Name_of_puzzle
 """
 
+import os
+import d2p1
+import d2p2
+
+FILE_NAME = 'd2i.txt'
+
 def main():
+
+	# get input from input file
+	with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), FILE_NAME), 'r') as input:
+		rounds = input.readlines()
+
+	print(d2p1.getTotalScore(rounds))
+
 	return 0
 
 if __name__ == '__main__':
