3df9ebffc5c61ef3c0e22599595494895d06c6ac
crohan99 <carson.rohan@gmail.com>
2022-12-05 22:41:19 -0600

    Modified template Modified to generate fewer files

--- util/prep_day.py
+++ util/prep_day.py
@@ -12,12 +12,12 @@ STATUS_OK = 200
 AOC_INPUT_URL = 'https://adventofcode.com/2022/day/?/input'
 URL = 'https://adventofcode.com/2022'
 COOKIES = {
-    'ru': 'userstring',
-    'session': 'cookiestring'
+    'ru': '53616c7465645f5f2c67d612a845a9fcdc07298538ebd2682ed70370db6628b9339c2b44f0ac9f6ddda8c328c019e2a7',
+    'session': '53616c7465645f5f23018bfd020806f650900995c24f0b6c2c15daeb7a7a0a96b8e7cead4e49d6e459eb9484221147c65c40d03de7200eb481312e500ea999eb'
 }
 
 INPUT_SIZE = 2
-DAY_TEMPLATE = '"""\nCarson Rohan\nAOC2022\nDay Part: Name_of_puzzle\n"""\n\ndef main():\n\treturn 0\n\nif __name__ == \'__main__\':\n\tmain()'
+DAY_TEMPLATE = '"""\nCarson Rohan\nAOC2022\nDay Part: Name_of_puzzle\n"""\n\nimport os\n\nFILE_NAME = \'d?i.txt\'\n\ndef main():\n\treturn 0\n\nclass Part1:\n\n\tdef blah():\n\t\treturn 0\n\nif __name__ == \'__main__\':\n\tmain()'
 
 def main():
 
@@ -54,9 +54,7 @@ def main():
     parentDir = os.path.dirname(os.path.abspath(__file__))
     dayDir = os.path.join(parentDir, r'../d%s'%(day))
     inputFile = os.path.join(dayDir, r'd%si.txt'%(day))
-    part1File = os.path.join(dayDir, r'd%sp1.py'%(day))
-    part2File = os.path.join(dayDir, r'd%sp2.py'%(day))
-    driverFile = os.path.join(dayDir, 'main.py')
+    dayFile = os.path.join(dayDir, r'd%s.py'%(day))
 
     if (not os.path.exists(dayDir)):
         os.mkdir(dayDir)
@@ -65,18 +63,9 @@ def main():
         with open(inputFile, 'w') as input:
             input.write(response.text)
 
-    if (not os.path.exists(part1File)):
-        with open(part1File, 'w') as p1:
+    if (not os.path.exists(dayFile)):
+        with open(dayFile, 'w') as p1:
             p1.write(DAY_TEMPLATE)
-
-    if (not os.path.exists(part2File)):
-        with open(part2File, 'w') as p2:
-            p2.write(DAY_TEMPLATE)
-
-    if (not os.path.exists(driverFile)):
-        with open(driverFile, 'w') as p2:
-            p2.write(DAY_TEMPLATE)
-
 
     return 0
 
