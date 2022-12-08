a7abec510203e9eb5e32e76ca48e4f3f6e6092bb
crohan99 <carson.rohan@gmail.com>
2022-12-07 23:22:42 -0600

    Added days

---
+++ d7/d7.py
@@ -0,0 +1,28 @@
+"""
+Carson Rohan
+AOC2022
+Day 7: Name_of_puzzle
+"""
+
+import os
+
+FILE_NAME = 'd7i.txt'
+
+def main():
+
+	with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), FILE_NAME)) as input:
+		fileInput = input.readlines()
+		return 0
+
+class Part1:
+
+	def blah():
+		return 0
+
+class Part2:
+
+	def blah():
+		return 0
+
+if __name__ == '__main__':
+	main()
