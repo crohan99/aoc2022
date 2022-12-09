c00f81dbe8edcf05309180a365aabcaa517ba6b8
crohan99 <carson.rohan@gmail.com>
2022-12-08 23:03:31 -0600

    Finished day 4 part 1

--- d4/d4.py
+++ d4/d4.py
@@ -6,18 +6,22 @@ Day 4: Name_of_puzzle
 
 import os
 
-FILE_NAME = 'd4i.txt'
+# FILE_NAME = 'd4i.txt'
+FILE_NAME = 'testinput.txt'
 
 def main():
 
 	with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), FILE_NAME)) as input:
 		fileInput = input.read().splitlines()
 
-	print(Part1.getDupeSectionCount(fileInput))
+	print(Part1().getDupeSectionCount(fileInput))
+	print(Part2().getDupeSectionCount(fileInput))
+
+	return 0
 
 class Part1:
 
-	def getDupeSectionCount(sections: list):
+	def getDupeSectionCount(self, sections: list):
 		sectionRangeElf1: tuple
 		sectionRangeElf2: tuple
 		dupeSectionsCount: int = 0
@@ -25,30 +29,45 @@ class Part1:
 		for sectionPair in sections:
 			sectionRangeElf1 = (int(sectionPair.split(',')[0].split('-')[0]), int(sectionPair.split(',')[0].split('-')[1]))
 			sectionRangeElf2 = (int(sectionPair.split(',')[1].split('-')[0]), int(sectionPair.split(',')[1].split('-')[1]))
-			print(sectionRangeElf1)
-			print(sectionRangeElf2)
-			print('')
 
-			if (Part1.isSubset(range(sectionRangeElf1[0], sectionRangeElf1[1]), range(sectionRangeElf2[0], sectionRangeElf2[1])) |
-				Part1.isSubset(range(sectionRangeElf2[0], sectionRangeElf2[1]), range(sectionRangeElf1[0], sectionRangeElf1[1]))):
-				print("dupe")
-				print('')
+			if (self.isBetween(sectionRangeElf1, sectionRangeElf2) or self.isBetween(sectionRangeElf2, sectionRangeElf1)):
 				dupeSectionsCount += 1
 
 		return dupeSectionsCount
 
-	def isSubset(range1, range2):
-		if (len(range1) == 0):
-			return range1.start in range2
+	def isBetween(self, set1: tuple, set2: tuple):
+		if (len(set1) == 2 and len(set2) == 2):
 
-		# return true if one of the set's extremes are between the other set's
-		return range1.start in range2 and range1[-1] in range2
+			if (set1[0] == set1[-1]):
+				return set1[0] >= set2[0] and set1[-1] <= set2[-1]
 
+			if (set1[0] >= set2[0] and set1[-1] <= set2[-1]):
+				return True
+
+		return False
 
 class Part2:
 
-	def blah():
-		return 0
+	def getDupeSectionCount(self, sections: list):
+		sectionRangeElf1: list
+		sectionRangeElf2: list
+		dupeSectionsCount: int = 0
+
+		for sectionPair in sections:
+			sectionRangeElf1 = [int(sectionPair.split(',')[0].split('-')[0]), int(sectionPair.split(',')[0].split('-')[1])]
+			sectionRangeElf2 = [int(sectionPair.split(',')[1].split('-')[0]), int(sectionPair.split(',')[1].split('-')[1])]
+
+			if (self.hasOverlap(sectionRangeElf1, sectionRangeElf2)):
+				dupeSectionsCount += 1
+
+		return dupeSectionsCount
+
+	def hasOverlap(self, set1: list, set2: list):
+		totalLen: int = len(set1) + len(set2)
+		# return True if the combined lists had duplicates removed
+		set1.extend(set2)
+		x = set(set1)
+		return len(set(set1)) != totalLen
 
 if __name__ == '__main__':
 	main()
